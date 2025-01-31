0e3772bd531324e67dcba868c2a27b4dd56f0b02
mohadesekeshtkar <143397162+KmohadeseK@users.noreply.github.com>
2025-01-30 20:14:38 -0800

    update scroll js file

--- js/scroll.js
+++ js/scroll.js
@@ -1,31 +1,13 @@
 'use strict';
 
-// select elements
-const section1 = document.querySelector('#home');
-const homeLink = document.querySelector('.home-link');
-const section2 = document.querySelector('#about');
-const aboutLink = document.querySelector('.about-link');
-const section3 = document.querySelector('#comments');
-const commentsLink = document.querySelector('.comments-link');
-const section4 = document.querySelector('#contact');
-const contactLink = document.querySelector('.contact-link');
+// select element
+const navLink = document.querySelectorAll('.nav-link');
 
-// scrolling contact link
-contactLink.addEventListener('click', function (e) {
-    section4.scrollIntoView({ behavior: 'smooth' });
+//*.................. Scrolling function ..................
+navLink.forEach(function (el) {
+    el.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = this.getAttribute('href');
+        document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+    })
 });
-
-// scrolling comments link
-commentsLink.addEventListener('click', function (e) {
-    section3.scrollIntoView({ behavior: 'smooth' });
-})
-
-// scrolling about link
-aboutLink.addEventListener('click', function (e) {
-    section2.scrollIntoView({ behavior: 'smooth' });
-})
-
-// scrolling home link
-homeLink.addEventListener('click', function (e) {
-    section1.scrollIntoView({ behavior: 'smooth' });
-})
